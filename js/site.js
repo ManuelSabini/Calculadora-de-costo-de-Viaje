d74a7228a2715f785733eb8d1a0da6033067700f
Manuel Sabini <manusabi13@gmail.com>
2022-12-11 23:40:21 -0300

    Se Añadieron Clases y Arrays para cumplir con la segunda entrega

--- js/site.js
+++ js/site.js
@@ -1,24 +1,66 @@
 //Simulador de gastos en combustibles en viajes
 alert("Bienvenido, el siguiente es un simulador de gastos de viaje.\n\nSe le realizaran una serie de consultas sobre la travesia que va a realizar y al finalizar, obtendrá una estimación del coste que tendrá su viaje.\n\nPara continuar presione ACEPTAR.");
 // Constantes:
-//Consumo de combustible (L cada 100 Km)
 /* Fuentes: 
 https://www.coches.net/consejos/que-gasta-mas-coche-o-moto
 https://surtidores.com.ar/el-precio-del-gnc-aumentara-en-las-estaciones-de-servicio-a-partir-de-noviembre/
-*/
-const consumoMoto = (3+6)/2;
-const consumoAutoNaftero = (4.5+10)/2;
-const consumoAutoGasolero = (4.0+9.2)/2;
-const consumoAutoGNC = 7.5;
+*/ 
+class Vehiculo{
+    constructor(){
+        this.consumovehiculo = 0;
+        this.preciocombustible = 0;
+        this.velocidadprom = 0;
+        //Consumo de combustible (L cada 100 Km)
+        this.consumoxvehiculo = [
+            {vehiculo: 1, consumo: (3+6)/2}, //Moto
+            {vehiculo: 2, consumo: (4.5+10)/2}, //Auto Naftero
+            {vehiculo: 3, consumo: (4.0+9.2)/2}, //Auto gasolero
+            {vehiculo: 4, consumo: 7.5}]; //Auto a GNC
+        
+        //Costo del litro/m3 de combustible ($/L (o M3))
+        this.precioxcombustible = [
+            {combustible: 1, precio: (150.9+184.4)/2}, //Promedio precio Naftas
+            {combustible: 2, precio: (169.9+218.9)/2}, //Promedio Precio Gasoil
+            {combustible: 3, precio: 60}]; //Promedio precio GNC
+        
+        //Velocidad promedio segun vehiculo (Km/h)
+        this.velocidadpromxvehiculo = [
+            {vehiculo: 1, velocidadprom: 90}, //Velocidad promedio Moto
+            {vehiculo: 2, velocidadprom: 110}];  //Velocidad promedio Auto
+    }
+    consumoStd(tipoVehiculo){
+        this.consumovehiculo = this.consumoxvehiculo.find((el) => el.vehiculo == tipoVehiculo)["consumo"];
+    }
+    precioStd(tipoCombustible){
+        this.preciocombustible = this.precioxcombustible.find((el) => el.combustible == tipoCombustible)["precio"];
+    }
+    velocidadProm(tipoVehiculo){
+        this.velocidadprom = this.velocidadpromxvehiculo.find((el) => el.vehiculo == tipoVehiculo)["velocidadprom"];
+    }
+}
 
-
-//Costo del litro/m3 de combustible ($/L (o M3))
-const precioNafta = (150.9+184.4)/2;
-const precioGasoil = (169.9+218.9)/2;
-const precioGNC = 60;
+class Calculos{
+    constructor(){
+        this.totalCombustible = 0;
+        this.totalGasto = 0;
+        this.totalTiempoDelViaje = 0;
+    }
+    combustibleNecesario(distancia,consumoStd) {
+        console.log("Combustible Necesario: "+(distancia * consumoStd /100).toFixed(2))
+        this.totalCombustible = (distancia * consumoStd /100).toFixed(2);
+    }
+    calculadoraGasto(combustibleNecesario, precioCombustible){ 
+        console.log("Gasto en combustible "+ (combustibleNecesario * precioCombustible).toFixed(2) )
+        this.totalGasto = (combustibleNecesario * precioCombustible).toFixed(2);
+    }
+    tiempo(velocidadProm, distancia){ 
+        console.log("Tiempo estimado: " + (distancia/velocidadProm).toFixed(2))
+        this.totalTiempoDelViaje = (distancia / velocidadProm).toFixed(2);
+    }
+}
 
 //Velocidad promedio (Km/h)
-const velocidadProm = 90
+//Remplazar por un objeto
 
 // Ingreso de Origen, destino y distancia
 function seleccionOrigen(){
@@ -33,68 +75,16 @@ function seleccionDistancia(){
     return parseInt(prompt("Indique la distancia entre Origen y destino (KM)"));
 }
 
-//Seleccion de tipo de vehiculo
-
+//Prompt seleccion de vehiculo
 function seleccionVehiculo() {
         let vehiculo = parseInt(prompt("Seleccione el vehiculo: \n\n1- Moto \n\n2- Auto"));
         return vehiculo;
 }
 
-function consumoStd(vehiculo) {
-    switch (vehiculo) {
-        case 9: //Moto
-        console.log("Consumo Std Moto: " + consumoMoto + "Km x L")
-        return consumoMoto;
-        case 1: //Auto Naftero
-        console.log("Consumo Std Auto Naftero: " + consumoAutoNaftero + "Km x L")
-        return consumoAutoNaftero;
-        case 2: //Auto Gasolero
-        console.log("Consumo Std Auto Gasolero: " + consumoAutoGasolero + "Km x L")
-        return consumoAutoGasolero;
-        case 3: //Auto GNC
-        console.log("Consumo Std Auto GNC: " + consumoAutoGNC + "Km x M3")
-        return consumoAutoGNC; 
-        default:
-            alert("Seleccione una opcion válida.");
-    }
-}
-
+//Prompt seleccion de Combustible
 function seleccionCombustible() {
         let vehiculo = parseInt(prompt("Seleccione el tipo de combustible: \n\n1- Nafta \n\n2- Gasoil \n\n3- GNC"));
         return vehiculo;
-}
-
-//Tipo de Combustible
-function precioCombustible(tipoCombustible){
-    switch (tipoCombustible) {
-        case 1: //Nafta
-        console.log("Nafta: "+ precioNafta +" $/L");
-        return precioNafta;
-        
-        case 2: //Gasoil
-        console.log("Gasoil: "+ precioGasoil +" $/L");
-        return precioGasoil;
-        
-        case 3: //GNC
-        console.log("GNC: "+ precioGNC +" $/M3");
-        return precioGNC;
-    }
-}
-
-//Calculo de gastos
-function combustibleNecesario(distancia,consumoStd) {
-    console.log("Combustible Necesario: "+(distancia * consumoStd /100).toFixed(2))
-    return (distancia * consumoStd /100).toFixed(2);
-}
-
-function calculadoraGasto(combustibleNecesario, precioCombustible){ 
-    console.log("Gasto en combustible "+ (combustibleNecesario * precioCombustible).toFixed(2) )
-    return (combustibleNecesario * precioCombustible).toFixed(2);
-}
-
-function tiempo(velocidadProm, distancia){ 
-    console.log("Tiempo estimado: " + (distancia/velocidadProm).toFixed(2))
-    return (distancia / velocidadProm).toFixed(2);
 }
 
 //Consultar Destino, Origen, Distancia
@@ -117,38 +107,57 @@ do {
 alert("Su viaje sera entre las ciudades de " + origen + " y " + destino + ".Distancia aproximada de "+ distancia + " Km");
 
 //Consulta Vehiculo
-let vehiculo
-
+let tipoVehiculo;
+let vehiculo = new Vehiculo;
+let estimacion = new Calculos;
 //Consulta Combustible
-let combustible;
-let precioCombustibleUtilizado;
+let tipoCombustible;
+/* let precioCombustibleUtilizado;
 let consumoVehiculo;
+ */
+let errores = false;
 
 do {
-    vehiculo = seleccionVehiculo();
-    if (vehiculo == 1){
-        combustible = 1;
-        precioCombustibleUtilizado = precioCombustible(combustible);
-        let consumo = 9;
-        
-        consumoVehiculo = consumoStd(consumo);
+    errores = false;
+    tipoVehiculo= seleccionVehiculo();
+    if (tipoVehiculo == 1){  //Si el tipo de vehiculo es MOTO por default se supone que la moto usa Nafta y que tiene un consumo propio de Moto naftera y una velocidad establecida
+        vehiculo.consumoStd(tipoVehiculo);
+        tipoCombustible = 1;
+        vehiculo.precioStd(tipoCombustible);
+        tipoVelocidad = tipoVehiculo;
+        vehiculo.velocidadProm(tipoVelocidad);
     
-    }else if (vehiculo == 2){ 
+    }else if (tipoVehiculo == 2){ //Si el vehiculo es Auto, el mismo puede ser Naftero, Gasolero o a GNC. Por ahora la velocidad esta fijada.
         do {
-            combustible = seleccionCombustible();
-            precioCombustibleUtilizado = precioCombustible(combustible);
-            consumoVehiculo = consumoStd(combustible);
-        } while (isNaN(precioCombustibleUtilizado)||isNaN(consumoVehiculo));
+            errores = false;
+            tipoCombustible = seleccionCombustible();
+            //Seleccion del tipo de combustible utilizado y defincion del consumo
+            if (tipoCombustible == 1) {
+                vehiculo.consumoStd(2);
+            } else if (tipoCombustible == 2) {
+                vehiculo.consumoStd(3);
+            } else if (tipoCombustible == 3) {
+                vehiculo.consumoStd(4);
+            } else {
+                alert("Seleccione una opcion válida.");
+                errores = true;
+            }
+        } while (errores);
+        vehiculo.precioStd(tipoCombustible);
+        tipoVelocidad = tipoVehiculo;
+        vehiculo.velocidadProm(tipoVelocidad);
     }else{
         alert("Seleccione una Opcion Valida")
+        errores = true;
     }
-} while (isNaN(precioCombustibleUtilizado)||isNaN(consumoVehiculo));
+} while (errores);
 
-let totalCombustible = combustibleNecesario(distancia, consumoVehiculo);
+//Hace calculo de las estimaciones de costo, consumo y tiempo estimado de viaje
+estimacion.combustibleNecesario(distancia, vehiculo.consumovehiculo);
 
-let totalGasto= calculadoraGasto(totalCombustible, precioCombustibleUtilizado);
+estimacion.calculadoraGasto(estimacion.totalCombustible, vehiculo.preciocombustible);
 
-let totalTiempoDelViaje = tiempo(velocidadProm,distancia);
+estimacion.tiempo(vehiculo.velocidadprom,distancia);
 
 //Muestra resultado
-alert("Su viaje entre las ciudades de " + origen + " y " + destino + ". \n\nDistancia: "+ distancia + " Km.\n\nConsumo de combustible: "+ totalCombustible +" Litros (o M3)\n\n Costo: " + totalGasto +" Pesos Arg.\n\nPrecio del Litro de combustible (o M3): "+ precioCombustibleUtilizado +"$/L\n\nTiempo estimado: " + totalTiempoDelViaje + " Horas\n\n")
+alert("Su viaje entre las ciudades de " + origen + " y " + destino + ". \n\nDistancia: "+ distancia + " Km.\n\nConsumo de combustible: "+ estimacion.totalCombustible +" Litros (o M3)\n\n Costo: " + estimacion.totalGasto +" Pesos Arg.\n\nPrecio del Litro de combustible (o M3): "+ vehiculo.preciocombustible +"$/L\n\nTiempo estimado: " + estimacion.totalTiempoDelViaje + " Horas\n\n")
