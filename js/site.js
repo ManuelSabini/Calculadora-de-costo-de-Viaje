56ee3dc5c29853a05986bd3fda90b19bdf1c3a06
Manuel Sabini <manusabi13@gmail.com>
2023-01-08 16:21:40 -0300

    Se agrego DOM, Eventos, LocalStorage

--- js/site.js
+++ js/site.js
@@ -1,10 +1,13 @@
 //Simulador de gastos en combustibles en viajes
-alert("Bienvenido, el siguiente es un simulador de gastos de viaje.\n\nSe le realizaran una serie de consultas sobre la travesia que va a realizar y al finalizar, obtendrá una estimación del coste que tendrá su viaje.\n\nPara continuar presione ACEPTAR.");
+
 // Constantes:
 /* Fuentes: 
 https://www.coches.net/consejos/que-gasta-mas-coche-o-moto
 https://surtidores.com.ar/el-precio-del-gnc-aumentara-en-las-estaciones-de-servicio-a-partir-de-noviembre/
 */ 
+
+
+//Creacion de Objetos, con sus respectivos metodos
 class Viaje{
     constructor(){
         this.origen = undefined;
@@ -12,22 +15,13 @@ class Viaje{
         this.distancia = undefined;
     }
     nuevoOrigen(){
-        do {
-            this.origen = prompt("Indique la ciudad de Origen").toUpperCase();
-        } while (this.origen == "");        
+        this.origen = document.getElementById("desde").value.toUpperCase();   
     }
     nuevoDestino(){
-        do {
-            this.destino = prompt("Indique la ciudad de Destino").toUpperCase();
-        } while (this.destino == "");
+        this.destino = document.getElementById("hasta").value.toUpperCase();   
     }
     nuevaDistancia(){
-        do {
-            this.distancia = parseInt(prompt("Indique la distancia entre Origen y destino (KM)"));
-        } while (isNaN(this.distancia));
-    }
-    descripcionViaje(){
-        alert("Su viaje sera entre las ciudades de " + this.origen + " y " + this.destino + ".Distancia aproximada de "+ this.distancia + " Km");
+        this.distancia = parseInt(document.getElementById("distancia").value);
     }
 }
 
@@ -38,22 +32,29 @@ class Vehiculo{
         this.velocidadprom = 0;
         //Consumo de combustible (L cada 100 Km)
         this.consumoxvehiculo = [
-            {vehiculo: 1, consumo: (3+6)/2}, //Moto
-            {vehiculo: 2, consumo: (4.5+10)/2}, //Auto Naftero
-            {vehiculo: 3, consumo: (4.0+9.2)/2}, //Auto gasolero
-            {vehiculo: 4, consumo: 7.5}]; //Auto a GNC
+            {vehiculo: "MotoNafta", consumo: (3+6)/2}, //Moto
+            {vehiculo: "AutoNafta", consumo: (4.5+10)/2}, //Auto Naftero
+            {vehiculo: "AutoGasoil", consumo: (4.0+9.2)/2}, //Auto gasolero
+            {vehiculo: "AutoGNC", consumo: 7.5}]; //Auto a GNC
         
         //Costo del litro/m3 de combustible ($/L (o M3))
         this.precioxcombustible = [
-            {combustible: 1, precio: (150.9+184.4)/2}, //Promedio precio Naftas
-            {combustible: 2, precio: (169.9+218.9)/2}, //Promedio Precio Gasoil
-            {combustible: 3, precio: 60}]; //Promedio precio GNC
+            {combustible: "Nafta", precio: (150.9+184.4)/2}, //Promedio precio Naftas
+            {combustible: "Gasoil", precio: (169.9+218.9)/2}, //Promedio Precio Gasoil
+            {combustible: "GNC", precio: 60}]; //Promedio precio GNC
         
         //Velocidad promedio segun vehiculo (Km/h)
         this.velocidadpromxvehiculo = [
-            {vehiculo: 1, velocidadprom: 90}, //Velocidad promedio Moto
-            {vehiculo: 2, velocidadprom: 110}];  //Velocidad promedio Auto
+            {vehiculo: "Moto", velocidadprom: 90}, //Velocidad promedio Moto
+            {vehiculo: "Auto", velocidadprom: 110}];  //Velocidad promedio Auto
     }
+    tipoVehiculo(){
+        this.tipoTransporte = document.querySelector("#form2 input:checked").value;
+    }
+    tipoCombustible(){
+        this.tipoMotor = document.querySelector("#form3 input:checked").value;
+    }
+        
     consumoStd(tipoVehiculo){
         this.consumovehiculo = this.consumoxvehiculo.find((el) => el.vehiculo == tipoVehiculo)["consumo"];
     }
@@ -71,78 +72,144 @@ class Calculos{
         this.totalGasto = 0;
         this.totalTiempoDelViaje = 0;
     }
-    combustibleNecesario(distancia,consumoStd) {
-        console.log("Combustible Necesario: "+(distancia * consumoStd /100).toFixed(2))
-        this.totalCombustible = (distancia * consumoStd /100).toFixed(2);
+    combustibleNecesario(distancia, consumoStd) {
+        console.log("Combustible Necesario: "+(distancia * consumoStd /100).toFixed(2));
+        this.totalCombustible = (distancia * consumoStd /100);
     }
     calculadoraGasto(combustibleNecesario, precioCombustible){ 
-        console.log("Gasto en combustible "+ (combustibleNecesario * precioCombustible).toFixed(2) )
-        this.totalGasto = (combustibleNecesario * precioCombustible).toFixed(2);
+        console.log("Gasto en combustible "+ (combustibleNecesario * precioCombustible).toFixed(2));
+        this.totalGasto = parseInt(combustibleNecesario * precioCombustible);
     }
     tiempo(velocidadProm, distancia){ 
-        console.log("Tiempo estimado: " + (distancia/velocidadProm).toFixed(2))
+        console.log("Tiempo estimado: " + (distancia/velocidadProm).toFixed(2));
         this.totalTiempoDelViaje = (distancia / velocidadProm).toFixed(2);
     }
 }
 
-//Consultar Destino, Origen, Distancia
+//MODIFICACIONES DEL HTML
+function InsertarCajaVehiculo() { 
+    let botones = document.querySelector(".botones");
+    botones.remove();
+    let caja = document.createElement("div");
+        caja.className = "container";
+        caja.id = "cajaVehiculo";
+    caja.innerHTML =   `<h2>Seleccione el vehiculo</h2>
+                        <form id="form2">
+                            <input type="radio" class="form-check-label" id="moto" name="tipoVehiculo" value="Moto">
+                            <label class="form-check-label" for="moto">Moto</label><br>
+                            <input type="radio" class="form-check-label" id="auto" name="tipoVehiculo" value="Auto">
+                            <label for="auto" class="form-check-label">Auto</label><br>
+                            <div class="botones"> 
+                                <button type="submit" class="btn btn-primary" type="button">Siguiente</button>
+                            </div>
+                        </form>`
+    document.querySelector("main").appendChild(caja);
+}
 
+function InsertarCajaCombustible() { 
+    let botones = document.querySelector(".botones");
+    botones.remove();
+    let caja = document.createElement("div");
+    caja.className = "container";
+    caja.id = "cajaVehiculo";
+    caja.innerHTML =   `<h2>Seleccione el tipo de combustible</h2>
+                        <form id="form3">
+                            <input type="radio" class="form-check-label" id="nafta" name="tipoCombustible" value="Nafta">
+                            <label for="nafta" class="form-check-label">Nafta</label><br>
+                            <input type="radio" class="form-check-label" id="gasoil" name="tipoCombustible" value="Gasoil">
+                            <label for="gasoil" class="form-check-label">Gasoil</label><br>
+                            <input type="radio" class="form-check-label" id="gnc" name="tipoCombustible" value="GNC">
+                            <label for="gnc" class="form-check-label">GNC</label><br>
+                            <div class="botones">
+                                <button type="submit" class="btn btn-primary" type="button">Siguiente</button>
+                            </div>
+                        </form>`
+    document.querySelector("main").appendChild(caja);
+}
+
+function InsertarCajaRespuesta() { 
+    vehiculo.precioStd(vehiculo.tipoMotor);
+    vehiculo.velocidadProm(vehiculo.tipoTransporte);
+    vehiculo.consumoStd(vehiculo.tipoTransporte+vehiculo.tipoMotor);
+    estimacion.combustibleNecesario(viaje.distancia, vehiculo.consumovehiculo);
+    estimacion.calculadoraGasto(estimacion.totalCombustible, vehiculo.preciocombustible);
+    estimacion.tiempo(vehiculo.velocidadprom, viaje.distancia);
+    let botones = document.querySelector(".botones");
+    botones.remove();
+    let caja = document.createElement("div");
+    caja.className = "container";
+    caja.id = "cajaRespuesta";
+    caja.innerHTML =   `<p>Su viaje entre las ciudades de ${viaje.origen} y ${viaje.destino}.</p>
+                        <p>Distancia:  ${viaje.distancia} Km.</p>
+                        <p>Consumo de combustible: ${estimacion.totalCombustible} Litros (o M3)</p>
+                        <p>Costo: ${estimacion.totalGasto} Pesos Arg.</p>
+                        <p>Precio del Litro de combustible (o M3): ${vehiculo.preciocombustible} $/L.</p>
+                        <p>Tiempo estimado: ${estimacion.totalTiempoDelViaje} Horas</p>
+                        <p>Proximamente boton compartir</p>
+                        <p>Proximamente boton recomendar a un amigo</p>
+                        <button type="button" class="btn btn-danger" id="limpiar" type="reset">Limpiar</button>`
+    document.querySelector("main").appendChild(caja);
+}
+
+//Listeners y Funciones de cada Caja
+function listenerCajaTravesia(){ 
+    siguiente = document.querySelector("#form1 .btn-primary");
+    siguiente.addEventListener("click",(e) => {
+        e.preventDefault();
+        console.log("Hiciste click en siguiente");
+        viaje.nuevoOrigen();
+        viaje.nuevoDestino();
+        viaje.nuevaDistancia();
+        //guardar en Local Storage
+        let travesiaJSON = {"origen": viaje.origen, "destino": viaje.destino,"distancia": viaje.distancia};
+        localStorage.setItem("travesia", JSON.stringify(travesiaJSON));
+        //Insertar la siguiente caja
+        InsertarCajaVehiculo();
+        listenerCajaVehiculo();
+    })
+}
+
+function listenerCajaVehiculo(){
+    siguiente = document.querySelector("#form2 .btn-primary");
+    siguiente.addEventListener("click",(e) => {
+        e.preventDefault();
+        console.log("Hiciste click en siguiente2");
+        vehiculo.tipoVehiculo();
+        //guardar en Local Storage
+        vehiculoJSON = {"tipo": vehiculo.tipoTransporte};
+        localStorage.setItem("vehiculo", JSON.stringify(vehiculoJSON));
+        //Insertar la siguiente caja
+        InsertarCajaCombustible();
+        listenerCajaCombustible();
+    })
+}
+
+function listenerCajaCombustible(){
+    siguiente = document.querySelector("#form3 .btn-primary")
+    siguiente.addEventListener("click",(e) => {
+        e.preventDefault();
+        console.log("Hiciste click en siguiente3");
+        vehiculo.tipoCombustible();
+        //Guardar en LocalStorage
+        combustibleJSON = {"tipoCombustible": vehiculo.tipoMotor};
+        localStorage.setItem("tipoCombustible", JSON.stringify(combustibleJSON));
+        //Mostrar respuestas
+        InsertarCajaRespuesta();
+        listenerCajaRespuesta();
+    })
+}
+
+function listenerCajaRespuesta(){
+    limpiar = document.querySelector("#limpiar");
+    limpiar.addEventListener("click", location.reload.bind(location));
+}
+
+
+//Creacion de Objetos
 let viaje = new Viaje;
-viaje.nuevoOrigen();
-viaje.nuevoDestino();
-viaje.nuevaDistancia();
-viaje.descripcionViaje();
-
-//Consulta Vehiculo
-let tipoVehiculo;
 let vehiculo = new Vehiculo;
 let estimacion = new Calculos;
 
-//Consulta Combustible
-let tipoCombustible;
-let errores = false;
+let siguiente;
 
-do {
-    errores = false;
-    tipoVehiculo= parseInt(prompt("Seleccione el vehiculo: \n\n1- Moto \n\n2- Auto"));
-    if (tipoVehiculo == 1){  //Si el tipo de vehiculo es MOTO por default se supone que la moto usa Nafta y que tiene un consumo propio de Moto naftera y una velocidad establecida
-        vehiculo.consumoStd(tipoVehiculo);
-        tipoCombustible = 1;
-        vehiculo.precioStd(tipoCombustible);
-        tipoVelocidad = tipoVehiculo;
-        vehiculo.velocidadProm(tipoVelocidad);
-    
-    }else if (tipoVehiculo == 2){ //Si el vehiculo es Auto, el mismo puede ser Naftero, Gasolero o a GNC. Por ahora la velocidad esta fijada.
-        do {
-            errores = false;
-            tipoCombustible = parseInt(prompt("Seleccione el tipo de combustible: \n\n1- Nafta \n\n2- Gasoil \n\n3- GNC"));
-            //Seleccion del tipo de combustible utilizado y defincion del consumo
-            if (tipoCombustible == 1) {
-                vehiculo.consumoStd(2);
-            } else if (tipoCombustible == 2) {
-                vehiculo.consumoStd(3);
-            } else if (tipoCombustible == 3) {
-                vehiculo.consumoStd(4);
-            } else {
-                alert("Seleccione una opcion válida.");
-                errores = true;
-            }
-        } while (errores);
-        vehiculo.precioStd(tipoCombustible);
-        tipoVelocidad = tipoVehiculo;
-        vehiculo.velocidadProm(tipoVelocidad);
-    }else{
-        alert("Seleccione una Opcion Valida")
-        errores = true;
-    }
-} while (errores);
-
-//Hace calculo de las estimaciones de costo, consumo y tiempo estimado de viaje
-estimacion.combustibleNecesario(viaje.distancia, vehiculo.consumovehiculo);
-
-estimacion.calculadoraGasto(estimacion.totalCombustible, vehiculo.preciocombustible);
-
-estimacion.tiempo(vehiculo.velocidadprom,viaje.distancia);
-
-//Muestra resultado
-alert("Su viaje entre las ciudades de " + viaje.origen + " y " + viaje.destino + ". \n\nDistancia: "+ viaje.distancia + " Km.\n\nConsumo de combustible: "+ estimacion.totalCombustible +" Litros (o M3)\n\n Costo: " + estimacion.totalGasto +" Pesos Arg.\n\nPrecio del Litro de combustible (o M3): "+ vehiculo.preciocombustible +"$/L\n\nTiempo estimado: " + estimacion.totalTiempoDelViaje + " Horas\n\n");
+listenerCajaTravesia();
