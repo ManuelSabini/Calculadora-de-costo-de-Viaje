99355cb4b2a06caf8b763a7f289e6c83065debf5
Manuel Sabini <manusabi13@gmail.com>
2023-01-22 02:09:15 -0300

    Se mejoraron funciones que llaman a APIs

--- js/apis.js
+++ js/apis.js
@@ -3,25 +3,67 @@
 //API DE ARGENTINA
 //https://apis.datos.gob.ar/georef/api/localidades?nombre=25
 let respuesta
-function apiLocalidades(busqueda,localizacion){
-    fetch("https://apis.datos.gob.ar/georef/api/localidades?nombre="+busqueda)
+
+//Busca posibles resultados
+function apiLocalidades(busqueda){
+    fetch("https://apis.datos.gob.ar/georef/api/localidades?nombre="+busqueda+"&campos=nombre,id,provincia.nombre")
     .then( response => response.json())
     .then( data => respuesta = data)
     .then( ()=>{
-        console.log(respuesta);
-        localStorage.setItem(localizacion,JSON.stringify(respuesta.localidades))}
-        )
+        console.log(respuesta)
+        })
     .catch(() =>{
         respuesta = "La API no funciona, listado de ciudades no disponible";
         console.log(respuesta);
         })
     .finally(()=>{
         respuesta.localidades.forEach(element => {
-            console.log(`${element.nombre}, Provincia de ${element.provincia.nombre}` )
+            console.log(`${element.nombre}, Provincia de ${element.provincia.nombre} (${element.id})` )
     })})
 }
-    //https://github.com/Project-OSRM/osrm-backend/blob/master/docs/http.md
-    
-    //Probar este requestAnimationFrame
-    //http://router.project-osrm.org/table/v1/driving/13.388860,52.517037;13.397634,52.529407?annotations=distance,duration
-    
+
+//Busca las coordenadas del lugar elegido
+function apiInfoLocalidad(id,localizacion){
+    fetch("https://apis.datos.gob.ar/georef/api/localidades?id="+id+"&campos=nombre,id,provincia.nombre,centroide")
+        .then( response => response.json())
+        .then( data => respuesta = data)
+        .then( ()=>{
+            console.log(respuesta);
+            localStorage.setItem(localizacion,JSON.stringify(respuesta.localidades))}
+            )
+        .then(()=>{
+            respuesta.localidades.forEach(element => {
+                console.log(`${element.nombre}, Provincia de ${element.provincia.nombre} (${element.id})` )
+            })})
+        .catch(() =>{
+            respuesta = "La API no funciona, listado de ciudades no disponible";
+            console.log(respuesta);
+            localStorage.setItem(localizacion,JSON.stringify(respuesta))
+        })
+}
+
+//Calculadora de DISTANCIA usando la formaula de Haversine (Calcula la distancia lineal, la API de google es paga)
+//https://www.movable-type.co.uk/scripts/latlong.html
+
+//let origen = JSON.parse(localStorage.getItem("Origen")) 
+//let destino = JSON.parse(localStorage.getItem("Destino")) 
+
+//origen[0].centroide.lat
+//origen[0].centroide.lon
+//destino[0].centroide.lat
+//destino[0].centroide.lon
+
+function distancia(lat1,lon1,lat2,lon2){
+    const R = 6371e3; // metres
+    const φ1 = lat1 * Math.PI/180; // φ, λ in radians
+    const φ2 = lat2 * Math.PI/180;
+    const Δφ = (lat2-lat1) * Math.PI/180;
+    const Δλ = (lon2-lon1) * Math.PI/180;
+
+    const a = Math.sin(Δφ/2) * Math.sin(Δφ/2) +
+            Math.cos(φ1) * Math.cos(φ2) *
+            Math.sin(Δλ/2) * Math.sin(Δλ/2);
+    const c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1-a));
+
+    return R * c / 1000; // in Kilometros
+}
